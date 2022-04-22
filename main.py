31c55696483e154fa1ced3e72983c2dde551faf6
Sajiya Nagpal <sazianagpal9226@gmail.com>
2022-04-22 13:16:45 +0530

    added: main.py (A page to input/paste Youtube Link)

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/', methods =["GET", "POST"])
+def shortner():
+    if request.method == "POST":
+        time_stamp = datetime.now()
+
+        # getting input with name = fname in HTML form
+        url = request.form.get("url_input")
+        key = request.form.get("key_input")
+
+        if url == "":
+            return "error"
+        else:
+            if key == None:
+                user_ip = request.remote_addr
+                url_id = url.split("/")[3].replace("watch?v=", '')
+
+                # inserting into database
+                data = {"_id": time_stamp,
+                        "url": url_id,
+                        "user_ip": user_ip
+                        }
+
+                return f"Your short url is {str(url)}"
+            else:
+                str_encoded = (url, key)
+                str_decoded = (url, key)
+                return f"{str_encoded},  {str_decoded}"
+
+    return render_template("index.html")
